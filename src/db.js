8ca3b1cb1e310663e8d78f6e40aa35bfd1bb220e
Sebastian Quack <sebastianquack@gmail.com>
2018-07-13 22:28:06 +0200

    add env variable for mongo

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 try {
-	mongoose.connect('mongodb://localhost:27017/hypertexting-test', { useNewUrlParser: true });	
+	mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });	
 }
 catch(error) {
 	console.error(error);
